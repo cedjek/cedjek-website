8da4c03670f437d71e780144a5fed1cbe956b480
Jericho Mark Bolante <jekblnt@Jek-MacBook-Air.local>
2026-05-06 12:10:05 +0800

    new link

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Menu, X, Plus, Minus, Sparkles, Loader2, ShoppingCart, MapPin, Phone, C
 // --- CONFIGURATION ---
 const apiKey = ""; // api set wala pa nailagay ehe
 const INQUIRY_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwnpxWharoic53E9DGcUHgglNXCib23zyFLN9UOaLgp7MmY0G7rIygsuU53onDdDk3g/exec'; 
-const ORDERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby-nIJtBPyxnEQ_u61cakAhbhQlzXtfVkTngraI3M7tMGLf-4plRAW3sQLJiWIc3Z5mqw/exec';
+const ORDERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwN6uECdfxmpxIH1_cjAPf6tjQmA8icTdZ3hd6FYMvlMnOG8_RxkwGcVKLK_mUN31Qwxg/exec';
 
 // --- AI Integration with Gemini (Not yet done pang future eme)
 const generateWithGemini = async (prompt) => {
